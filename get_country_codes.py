b23eca27823a29694ad4280c2ed79932f32a629f
Steve Porter <sjporter399@gmail.com>
2019-04-27 10:51:08 -0600

    Update get_country_codes.py

--- get_country_codes.py
+++ get_country_codes.py
@@ -1,3 +1,13 @@
 def get_country_codes(param):
     # Your code here. Maybe try pseudocoding this one out?
-    return ""
+    alpha_list = []
+    converted_string = ""
+    for char in prices:
+        if char.isalpha():
+            alpha_list.append(char)
+    
+    for i in range(int(len(alpha_list)/2)):
+        
+         converted_string  += "".join(alpha_list[i*2:(i*2) + 2]) + ", "
+        
+    return converted_string
